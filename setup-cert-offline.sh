36225a408ef34c9171ed54fd7182714a06367ca7
Ibrahim Olukorede Folarin <ifolarin@webbasedsol.com>
2018-01-28 00:53:57 +0100

    added imap and smtp to certificate request

--- setup-cert-offline.sh
+++ setup-cert-offline.sh
@@ -18,7 +18,7 @@ mkdir -p config/letsencrypt/etc
 cd config/letsencrypt
 
 echo "Getting certificates..."
-GetCert -d mail.example.org -d example.org -d www.example.org -d lssp.example.org -d ldap.example.org
+GetCert -d mail.example.org -d example.org -d www.example.org -d lssp.example.org -d ldap.example.org -d imap.example.org -d smtp.example.org
 cd - 
 
 
