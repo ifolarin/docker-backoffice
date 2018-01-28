1ed155832ae0a90ba63c90720e33523585b61be5
Ibrahim Olukorede Folarin <ifolarin@webbasedsol.com>
2018-01-28 01:06:04 +0100

    added imap and smtp to cert request and cleaned up formatting

--- setup-cert.sh
+++ setup-cert.sh
@@ -3,21 +3,21 @@
 #docker pull haocen/certbot
 
 GetCert(){
-				docker run -it \
-								--rm \
-								-v $(pwd)/config/letsencrypt/etc:/etc/letsencrypt \
-								-v $(pwd)/config/letsencrypt/lib:/var/lib/letsencrypt \
-								-v $(pwd)/config/letsencrypt/www:/var/www/.well-known \
-							  haocen/certbot -t certonly --webroot -w /var/www \
-								--keep-until-expiring \
-								$@
+  docker run -it \
+  --rm \
+  -v $(pwd)/config/letsencrypt/etc:/etc/letsencrypt \
+  -v $(pwd)/config/letsencrypt/lib:/var/lib/letsencrypt \
+  -v $(pwd)/config/letsencrypt/www:/var/www/.well-known \
+  haocen/certbot -t certonly --webroot -w /var/www \
+  --keep-until-expiring \
+  $@
 }
 
 echo "Getting certificates..."
-GetCert -d mail.example.org -d example.org -d www.example.org -d lssp.example.org -d ldap.example.org
+GetCert -d mail.example.org -d example.org -d www.example.org -d lssp.example.org -d ldap.example.org -d imap.example.org -d smtp.example.org
 
 
 
 
 
-								
+                
