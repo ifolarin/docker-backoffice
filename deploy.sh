fd68e775ae43f007d049f738ffa1cb0577564834
Ibrahim Olukorede Folarin <ifolarin@webbasedsol.com>
2018-02-13 11:51:13 +0100

    fixed missing period

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ cd -
 #Clone and build s3-backup
 git clone https://github.com/ifolarin/docker-s3-cron-backup.git
 cd docker-s3-cron-backup
-docker build -t webbasedsol/s3-cron-backup:0.1.0
+docker build -t webbasedsol/s3-cron-backup:0.1.0 .
 docker tag  webbasedsol/s3-cron-backup:0.1.0 webbasedsol/s3-cron-backup:latest
 cd -
 
